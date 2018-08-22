1925f06422516294f7cb735121e8b604e21b9a62
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2018-08-22 23:18:25 +0100

    Add src code folder

---
+++ src/NovaResourceMakerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+namespace Inani\NovaResourceMaker;
+
+use Illuminate\Support\ServiceProvider;
+
+class NovaResourceMakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Boot What is needed
+     *
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the poll writer instance.
+     *
+     * @return void
+     */
+    protected function registerPollWriter()
+    {
+    }
+}
